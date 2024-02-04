c4ab8f1328c1c5102a08a50e8a5d2a5c895e0c52
Hun <ksh26271@naver.com>
2024-02-04 23:01:56 +0900

    Initial commit

---
+++ WHOA_iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
