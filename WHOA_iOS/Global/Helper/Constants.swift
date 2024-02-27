4aeeb5387659197d3020bf3f183ce42db6a31ca0
Hun <ksh26271@naver.com>
2024-02-28 04:09:06 +0900

    feat: NameSpace를 지정할 Constants 추가

---
+++ WHOA_iOS/Global/Helper/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 2/28/24.
+//
+
+import Foundation
+
+//MARK: - Name Space
+
+enum CellIdentifier {
+    static let hashTagCellIdentifier = "HashTagCollectionViewCell"
+}
