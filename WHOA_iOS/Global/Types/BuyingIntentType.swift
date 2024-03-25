2b484fbee4ca2a3362b7521684ecb2dd9327060e
Hun <ksh26271@naver.com>
2024-03-25 15:59:05 +0900

    fix: 구매 목적 Enum Case 수정

--- WHOA_iOS/Global/Types/BuyingIntentType.swift
+++ WHOA_iOS/Global/Types/BuyingIntentType.swift
@@ -9,14 +9,12 @@ import Foundation
 
 /// 꽃 구매 목적을 정리한 Enum입니다.
 enum BuyingIntentType: String {
-    case 회갑 = "회갑 / 칠순 / 연회"
-    case 생일 = "생일 / 기념일"
-    case 추모 = "추모 / 근조 / 장례식"
-    case 결혼식 = "결혼식 / 취임식"
-    case 애정표현 = "애정표현"
-    case 승진 = "승진 / 취업"
-    case 전시 = "전시 / 공연"
-    case 수상 = "수상 / 입학 / 졸업"
-    case 사과 = "사과 / 용서 / 후회"
-    case 행사 = "행사 / 개업식"
+    case affection = "애정표현/고백/기념일"
+    case birthday = "생일/생신"
+    case gratitude = "감사표현"
+    case propose = "프로포즈/결혼식"
+    case party = "연회/행사/생일"
+    case employment = "취업/개업식"
+    case promotion = "승진/취임식"
+    case friendship = "우정/추억/청춘"
 }
