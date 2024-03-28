929bc971f163a818606790b10d379a59601d38fd
Hun <ksh26271@naver.com>
2024-03-28 16:28:27 +0900

    feat: CustomFont Extension 추가

---
+++ WHOA_iOS/Global/Extensions/UIFont+.swift
@@ -0,0 +1,19 @@
+//
+//  UIFont+.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 3/28/24.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    enum Family: String {
+        case Black, Bold, ExtraBold, ExtraLight, Light, Medium, Regular, SemiBold, Thin
+    }
+    
+    static func Pretendard(size: CGFloat = 10, family: Family = .Regular) -> UIFont {
+        return UIFont(name: "Pretendard-\(family)", size: size)!
+    }
+}
