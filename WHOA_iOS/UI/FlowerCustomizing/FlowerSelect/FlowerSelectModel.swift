2c43f8a5fa158dcdb1e6bbae7422556df0e620b0
Hun <ksh26271@naver.com>
2024-04-05 06:36:58 +0900

    feat: FlowerKeywordModel 선언

---
+++ WHOA_iOS/UI/FlowerCustomizing/FlowerSelect/FlowerSelectModel.swift
@@ -0,0 +1,14 @@
+//
+//  FlowerSelectModel.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 4/5/24.
+//
+
+import Foundation
+
+struct FlowerKeywordModel {
+    let flowerName: String
+    let flowerImage: String
+    let flowerKeyword: [String]
+}
