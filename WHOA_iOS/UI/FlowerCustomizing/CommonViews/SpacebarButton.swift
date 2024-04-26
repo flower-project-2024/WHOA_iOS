310dcd4486f025a497f82f4df1ceb11be4225e0f
Hun <ksh26271@naver.com>
2024-04-26 21:15:30 +0900

    feat: SpacebarButton구현

---
+++ WHOA_iOS/UI/FlowerCustomizing/CommonViews/SpacebarButton.swift
@@ -0,0 +1,54 @@
+//
+//  SpacebarButton.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 4/26/24.
+//
+
+import UIKit
+
+class SpacebarButton: UIButton {
+    
+    // MARK: - Initialization
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        configureButton(title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    private func configureButton(_ title: String) {
+        self.configuration = configure(title: title, isSelected: false)
+        self.contentHorizontalAlignment = .leading
+        
+        self.layer.cornerRadius = 8
+        self.layer.masksToBounds = true
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.clear.cgColor
+    }
+    
+    func configure(title: String, isSelected: Bool) -> UIButton.Configuration {
+        var config = UIButton.Configuration.filled()
+        config.titleAlignment = .leading
+        config.baseBackgroundColor = isSelected ? .second1.withAlphaComponent(0.2) : .gray2
+        config.baseForegroundColor = .primary
+        
+        // Title
+        var attString = AttributedString(title)
+        attString.font = isSelected ? .Pretendard(size: 16, family: .SemiBold) : .Pretendard(size: 16)
+        config.attributedTitle = attString
+        
+        // Image
+        let colorsConfig = UIImage.SymbolConfiguration(paletteColors: isSelected ? [.secondary3, .secondary3] : [.gray2, .gray5])
+        config.image = UIImage(systemName: "button.programmable", withConfiguration: colorsConfig)
+        config.imagePlacement = .leading
+        config.imagePadding = 8
+        
+        return config
+    }
+}
