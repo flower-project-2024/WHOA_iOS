40e363d249f6bdcdca73331bbe797baca563ea64
Suyeon <syhhwang1231@gmail.com>
2024-05-03 20:59:00 +0900

    Feat: 배너에서 상세 페이지로 넘어갈 때 타이머 비활성

--- WHOA_iOS/UI/Home/HomeViewController.swift
+++ WHOA_iOS/UI/Home/HomeViewController.swift
@@ -188,26 +188,26 @@ extension HomeViewController : UITableViewDelegate {
 }
 
 // MARK: - Extensions; ScrollView
-extension UIScrollView {
-    func updateContentSize() {
-        let unionCalculatedTotalRect = recursiveUnionInDepthFor(view: self)
-        
-        // 계산된 크기로 컨텐츠 사이즈 설정
-        self.contentSize = CGSize(width: self.frame.width, height: unionCalculatedTotalRect.height+50)
-    }
-    
-    private func recursiveUnionInDepthFor(view: UIView) -> CGRect {
-        var totalRect: CGRect = .zero
-        
-        // 모든 자식 View의 컨트롤의 크기를 재귀적으로 호출하며 최종 영역의 크기를 설정
-        for subView in view.subviews {
-            totalRect = totalRect.union(recursiveUnionInDepthFor(view: subView))
-        }
-        
-        // 최종 계산 영역의 크기를 반환
-        return totalRect.union(view.frame)
-    }
-}
+//extension UIScrollView {
+//    func updateContentSize() {
+//        let unionCalculatedTotalRect = recursiveUnionInDepthFor(view: self)
+//        
+//        // 계산된 크기로 컨텐츠 사이즈 설정
+//        self.contentSize = CGSize(width: self.frame.width, height: unionCalculatedTotalRect.height+50)
+//    }
+//    
+//    private func recursiveUnionInDepthFor(view: UIView) -> CGRect {
+//        var totalRect: CGRect = .zero
+//        
+//        // 모든 자식 View의 컨트롤의 크기를 재귀적으로 호출하며 최종 영역의 크기를 설정
+//        for subView in view.subviews {
+//            totalRect = totalRect.union(recursiveUnionInDepthFor(view: subView))
+//        }
+//        
+//        // 최종 계산 영역의 크기를 반환
+//        return totalRect.union(view.frame)
+//    }
+//}
 
 // MARK: - Extensions; CollectionView
 extension HomeViewController: UICollectionViewDelegate {
@@ -242,6 +242,8 @@ extension HomeViewController: UICollectionViewDataSource {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TodaysFlowerViewCell.identifier, for: indexPath) as! TodaysFlowerViewCell
             cell.buttonCallbackMethod = { [weak self] in
                 print("Call Back Method")
+                self?.timer?.invalidate()
+                self?.timer = nil
                 self?.navigationController?.pushViewController(FlowerDetailViewController(), animated: true)
             }
             return cell
