2b1b02f55753aa5af7d96479d2c6ce7c09197eba
Hun <ksh26271@naver.com>
2024-05-09 10:55:42 +0900

    feat: FlowerKeywordModel 객체 비교 기능 추가

--- WHOA_iOS/UI/FlowerCustomizing/FlowerSelect/FlowerSelectModel.swift
+++ WHOA_iOS/UI/FlowerCustomizing/FlowerSelect/FlowerSelectModel.swift
@@ -12,3 +12,11 @@ struct FlowerKeywordModel {
     let flowerImage: String
     let flowerKeyword: [String]
 }
+
+extension FlowerKeywordModel: Equatable {
+    static func == (lhs: FlowerKeywordModel, rhs: FlowerKeywordModel) -> Bool {
+        return lhs.flowerName == rhs.flowerName &&
+               lhs.flowerImage == rhs.flowerImage &&
+               lhs.flowerKeyword == rhs.flowerKeyword
+    }
+}
