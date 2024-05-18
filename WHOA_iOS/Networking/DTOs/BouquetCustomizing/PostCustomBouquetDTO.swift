30400125991f3ea625ca0a51ba609bd6be4a014f
Hun <ksh26271@naver.com>
2024-05-18 17:51:28 +0900

    feat: PostCustomBouquetDTO 오류처리 추가

--- WHOA_iOS/Networking/DTOs/BouquetCustomizing/PostCustomBouquetDTO.swift
+++ WHOA_iOS/Networking/DTOs/BouquetCustomizing/PostCustomBouquetDTO.swift
@@ -11,10 +11,41 @@ struct PostCustomBouquetDTO: Codable {
     let timestamp: String
     let success: Bool
     let status: Int
-    let data: BouquetIdData
+    let data: ResponseResult
 }
 
-struct BouquetIdData: Codable {
+struct BouquetSuccess: Codable {
     let bouquetId: Int
 }
 
+struct BouquetFailure: Codable {
+    let errorType: String
+    let message: String
+}
+
+enum ResponseResult: Codable {
+    case success(BouquetSuccess)
+    case failure(BouquetFailure)
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let data = try? container.decode(BouquetSuccess.self) {
+            self = .success(data)
+        } else if let data = try? container.decode(BouquetFailure.self) {
+            self = .failure(data)
+        } else {
+            throw NetworkError.unableToDecode
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .success(let data):
+            try container.encode(data)
+        case .failure(let data):
+            try container.encode(data)
+        }
+    }
+}
