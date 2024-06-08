daec5f74338546b1d5b8763ba9e6085b27a22c83
Hun <ksh26271@naver.com>
2024-06-08 23:49:47 +0900

    feat: NumberOfColorsBox 커스텀 뷰 구현

---
+++ WHOA_iOS/UI/FlowerCustomizing/FlowerColorPicker/Views/NumberOfColorsBox.swift
@@ -0,0 +1,69 @@
+//
+//  NumberOfColorsBox.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 6/7/24.
+//
+
+import UIKit
+import SnapKit
+
+class NumberOfColorsBox: UIView {
+    
+    // MARK: - Properties
+    
+    private let colorSelectionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "조합"
+        label.textColor = .gray9
+        label.font = UIFont.Pretendard(size: 16, family: .SemiBold)
+        return label
+    }()
+    
+    private let chevronImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.down")
+        imageView.tintColor = .black
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Functions
+    
+    private func setupUI() {
+        backgroundColor = .gray2
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+        
+        addSubview(colorSelectionLabel)
+        addSubview(chevronImageView)
+        
+        setupAutoLayout()
+    }
+}
+
+extension NumberOfColorsBox {
+    private func setupAutoLayout() {
+        colorSelectionLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(12)
+        }
+        
+        chevronImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+    }
+}
