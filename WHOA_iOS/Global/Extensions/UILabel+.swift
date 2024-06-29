b00c15648e9d5386f1f7c8e13239b86f64f1eb30
Suyeon <syhhwang1231@gmail.com>
2024-06-30 03:29:43 +0900

    Feat: linespacing 설정하는 UILabel Extension 추가

---
+++ WHOA_iOS/Global/Extensions/UILabel+.swift
@@ -0,0 +1,22 @@
+//
+//  UILabel+.swift
+//  WHOA_iOS
+//
+//  Created by Suyeon Hwang on 6/30/24.
+//
+
+import UIKit
+
+extension UILabel {
+    func setLineSpacing(spacing: CGFloat) {
+        guard let text = text else { return }
+
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = spacing
+        attributeString.addAttribute(.paragraphStyle,
+                                     value: style,
+                                     range: NSRange(location: 0, length: attributeString.length))
+        attributedText = attributeString
+    }
+}
