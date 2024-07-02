efe2652730fe21406ac661c2afaaf0924813d316
Hun <ksh26271@naver.com>
2024-07-03 02:04:02 +0900

    feat: 요구서 조회 시 요구사항 이미지, 꽃 데이터 보이도록 구현

--- WHOA_iOS/Networking/DTOs/BouquetCustomizing/BouquetDetailDTO.swift
+++ WHOA_iOS/Networking/DTOs/BouquetCustomizing/BouquetDetailDTO.swift
@@ -24,30 +24,57 @@ struct BouquetDetail: Codable {
     let substitutionType: String
     let wrappingType: String
     let priceRange: String
-    let requirement: String
-    //    let imagePaths: [String]
-    //    let flowerInfoList: [String]
+    let requirement: String?
+    let imgPaths: [String]
+    let flowerInfoList: [FlowerInfoList]
 }
 
+struct FlowerInfoList: Codable {
+    let flowerImageUrl: String
+    let flowerName: String
+    let flowerLanguage: String
+}
 
 extension BouquetDetailDTO {
-    static func convertBouquetDetailDTOToModel(_ DTO: BouquetDetailDTO) -> CustomizingSummaryModel {
+    static func convertBouquetDetailDTOToModel(_ DTO: BouquetDetailDTO, completion: @escaping (CustomizingSummaryModel?) -> Void) {
         var colors = DTO.data.colorName.components(separatedBy: ",")
         
         if let pointColor = DTO.data.pointColor {
             colors.insert(pointColor, at: 0)
         }
         
-        return CustomizingSummaryModel(
-            purpose: PurposeType(rawValue: DTO.data.purpose) ?? .affection,
-            numberOfColors: NumberOfColorsType(rawValue: DTO.data.colorType) ?? .oneColor,
-            colors: colors,
-            flowers: [],
-            alternative: AlternativesType(rawValue: DTO.data.substitutionType) ?? .colorOriented,
-            assign: Assign(packagingAssignType: PackagingAssignType(rawValue: DTO.data.wrappingType) ?? .myselfAssign, text: DTO.data.wrappingType),
-            priceRange: DTO.data.priceRange,
-            requirement: Requirement(text: DTO.data.requirement, imageFiles: [])
-        )
+        let flowers = DTO.data.flowerInfoList.map { Flower(
+            id: nil, photo: $0.flowerImageUrl,
+            name: $0.flowerName,
+            hashTag: $0.flowerLanguage.components(separatedBy: ", ")
+        ) }
+        
+        let dispatchGroup = DispatchGroup()
+        var imageFiles: [ImageFile] = []
+        
+        for imgPath in DTO.data.imgPaths {
+            dispatchGroup.enter()
+            urlStringToImageFile(urlString: imgPath, filename: "RequirementImage") { imageFile in
+                if let imageFile = imageFile {
+                    imageFiles.append(imageFile)
+                }
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            let model = CustomizingSummaryModel(
+                purpose: PurposeType(rawValue: DTO.data.purpose) ?? .affection,
+                numberOfColors: NumberOfColorsType(rawValue: DTO.data.colorType) ?? .oneColor,
+                colors: colors,
+                flowers: flowers,
+                alternative: AlternativesType(rawValue: DTO.data.substitutionType) ?? .colorOriented,
+                assign: Assign(packagingAssignType: PackagingAssignType(rawValue: DTO.data.wrappingType) ?? .myselfAssign, text: DTO.data.wrappingType),
+                priceRange: DTO.data.priceRange,
+                requirement: Requirement(text: DTO.data.requirement, imageFiles: imageFiles)
+            )
+            completion(model)
+        }
     }
 }
 
