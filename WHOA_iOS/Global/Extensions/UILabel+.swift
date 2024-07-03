4ce296bb575de3bdf3a17a71c6370d887fd967cb
Suyeon <syhhwang1231@gmail.com>
2024-07-03 17:43:25 +0900

    Fix: UILabel에 line spacing 적용하는 함수 % 값으로 적용할 수 있도록 수정

--- WHOA_iOS/Global/Extensions/UILabel+.swift
+++ WHOA_iOS/Global/Extensions/UILabel+.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 extension UILabel {
-    func setLineSpacing(spacing: CGFloat) {
+    
+    /// UILabel에 line height를 적용하는 함수입니다.
+    /// - Parameter lineHeight: 적용하려는 line height 백분율 값 (피그마에 있는 값)
+    func setLineHeight(lineHeight: CGFloat) {
         guard let text = text else { return }
 
         let attributeString = NSMutableAttributedString(string: text)
         let style = NSMutableParagraphStyle()
-        style.lineSpacing = spacing
+        let lineheight = font.lineHeight / 100 * (lineHeight - 100) / 4  //font size * multiple        
+        style.lineSpacing = lineheight
         attributeString.addAttribute(.paragraphStyle,
                                      value: style,
                                      range: NSRange(location: 0, length: attributeString.length))
