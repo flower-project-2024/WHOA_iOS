eb3b08edb35e33f714b12d60a2532a57480d3c44
Suyeon <syhhwang1231@gmail.com>
2024-08-29 22:57:39 +0900

    Refactor: 그림자 적용 extension 메소드 파라미터명 변경

--- WHOA_iOS/Global/Extensions/CALayer+.swift
+++ WHOA_iOS/Global/Extensions/CALayer+.swift
@@ -11,13 +11,13 @@ extension CALayer {
     func applyShadow(
         color: UIColor = .black,
         alpha: Float = 0.5,
-        x: CGFloat = 0,
-        y: CGFloat = 2,
+        width: CGFloat = 0,
+        height: CGFloat = 2,
         blur: CGFloat = 4
     ) {
         shadowColor = color.cgColor
         shadowOpacity = alpha
-        shadowOffset = CGSize(width: x, height: y)
+        shadowOffset = CGSize(width: width, height: height)
         shadowRadius = blur / 2.0
     }
 }
