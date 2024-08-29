5a4928416d70bbf65c650579a703a98f95389ed8
Suyeon <syhhwang1231@gmail.com>
2024-08-29 23:18:24 +0900

    Feat: 그림자 적용된 segment control 구현

--- WHOA_iOS/UI/MyPage/MyPageViewController.swift
+++ WHOA_iOS/UI/MyPage/MyPageViewController.swift
@@ -13,6 +13,7 @@ final class MyPageViewController: UIViewController, CustomAlertViewControllerDel
     
     private let viewModel = BouquetListModel()
     private let cellVerticalSpacing: CGFloat = 8
+    private let underlineViewWidth: CGFloat = (UIScreen.main.bounds.width - 20.adjusted(basedOnWidth: 390) * 2 - 15.adjusted(basedOnWidth: 390) * 2) / 3
     private var noRequest = true
     var customizingCoordinator: CustomizingCoordinator?
     
@@ -20,10 +21,52 @@ final class MyPageViewController: UIViewController, CustomAlertViewControllerDel
         
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "저장된 요구서"
+        label.text = "꽃다발 요구서"
         label.font = UIFont.Pretendard(size: 20, family: .SemiBold)
         label.textAlignment = .left
         return label
+    }()
+    
+    private let segmentContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.masksToBounds = false
+//        view.layer.applyShadow(y: 47.,blur: 12)
+//        view.layer.shadowOffset = CGSize(width: 0, height: 4)
+//        view.layer.shadowColor = UIColor.black.cgColor
+//        view.layer.shadowRadius = 8  // 반경?
+//        view.layer.shadowOpacity = 0.3  // alpha값입니다.
+        return view
+    }()
+    
+    private let segmentControl: UISegmentedControl = {
+        let segment = UISegmentedControl()
+        segment.insertSegment(withTitle: "전체", at: 0, animated: true)
+        segment.insertSegment(withTitle: "저장된 요구서", at: 1, animated: true)
+        segment.insertSegment(withTitle: "제작 완료", at: 2, animated: true)
+        segment.selectedSegmentIndex = 0
+        
+        segment.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.gray06,
+            NSAttributedString.Key.font: UIFont.Pretendard(size: 16)],
+                                       for: .normal)
+        segment.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont.Pretendard(size: 16, family: .SemiBold)],
+                                       for: .selected)
+        
+        segment.selectedSegmentTintColor = .clear
+        segment.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+        segment.setBackgroundImage(UIImage(), for: .selected, barMetrics: .default)
+        segment.setBackgroundImage(UIImage(), for: .highlighted, barMetrics: .default)
+        segment.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        return segment
+    }()
+    
+    private let segmentUnderLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
     }()
     
     private lazy var tableView: UITableView = {
@@ -54,6 +97,9 @@ final class MyPageViewController: UIViewController, CustomAlertViewControllerDel
         setupNavigation()
         addViews()
         setupConstraints()
+        
+        // segment control에 그림자 추가
+        segmentContainerView.layer.applyShadow(alpha: 0.04, height: 6, blur: 12 / UIScreen.main.scale)
     }
     
     // MARK: - Functions
@@ -64,12 +110,37 @@ final class MyPageViewController: UIViewController, CustomAlertViewControllerDel
     }
     
     private func addViews() {
+        view.addSubview(segmentContainerView)
+        segmentContainerView.addSubview(segmentControl)
+//        view.addSubview(segmentControl)
+        view.addSubview(segmentUnderLineView)
         view.addSubview(tableView)
     }
     
     private func setupConstraints() {
+        segmentContainerView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+            make.height.equalTo(47)
+        }
+        
+        segmentControl.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20.adjusted(basedOnWidth: 390))
+            make.trailing.equalToSuperview().inset(20.adjusted(basedOnWidth: 390))
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        segmentUnderLineView.snp.makeConstraints { make in
+            make.bottom.equalTo(segmentControl.snp.bottom)
+            make.height.equalTo(4)
+            make.width.equalTo(underlineViewWidth)
+            make.leading.equalTo(segmentControl.snp.leading)
+        }
+        
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(6)
+            make.top.equalTo(segmentControl.snp.bottom).offset(36)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
             make.bottom.equalToSuperview()
