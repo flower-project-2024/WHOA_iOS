628caf410257a381f8d15296037f6ce47469ff08
Hun <ksh26271@naver.com>
2024-09-04 07:08:32 +0900

    feat: 커스터마이징 종료 시 BouquetData 리셋 로직 구현

--- WHOA_iOS/UI/FlowerCustomizing/CommonViews/CustomExitAlertViewController.swift
+++ WHOA_iOS/UI/FlowerCustomizing/CommonViews/CustomExitAlertViewController.swift
@@ -109,6 +109,7 @@ final class CustomExitAlertViewController: UIViewController {
     @objc
     private func exitButtonTapped() {
         dismiss(animated: true) { [weak self] in
+            DataManager.shared.reset()
             self?.currentVC?.tabBarController?.selectedIndex = 0
             self?.currentVC?.navigationController?.popToRootViewController(animated: true)
             
