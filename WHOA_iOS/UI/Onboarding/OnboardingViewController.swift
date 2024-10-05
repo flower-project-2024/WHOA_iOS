ee387aef14f6e22ed7a4f4d95796862f04de7c05
Hun <ksh26271@naver.com>
2024-10-05 19:38:58 +0900

    fix: 온보딩 관련 UI OnboardingView로 분리

--- WHOA_iOS/UI/Onboarding/OnboardingViewController.swift
+++ WHOA_iOS/UI/Onboarding/OnboardingViewController.swift
@@ -16,7 +16,6 @@ final class OnboardingViewController: UIViewController {
     /// Metrics
     private enum Metric {
         static let verticalSpacing = 43
-        static let scrollViewHeightMultiplier = 587.0 / 375.0
     }
     
     /// Attributes
@@ -27,50 +26,11 @@ final class OnboardingViewController: UIViewController {
     
     // MARK: - Properties
     
-    private let currentPageSubject: CurrentValueSubject<Int, Never> = .init(0)
     private var cancellables = Set<AnyCancellable>()
-    
-    private let onboardingImageNames = [
-        UIImage.onboardingStep01,
-        UIImage.onboardingStep02,
-        UIImage.onboardingStep03,
-        UIImage.onboardingStep04
-    ]
     
     // MARK: - UI
     
-    private let pageControl: UIPageControl = {
-        let pageControl = UIPageControl()
-        pageControl.currentPage = 0
-        pageControl.pageIndicatorTintColor = .gray04
-        pageControl.currentPageIndicatorTintColor = .secondary03
-        pageControl.numberOfPages = 4
-        return pageControl
-    }()
-    
-    private lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.isPagingEnabled = true
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.bounces = false
-        scrollView.delegate = self
-        return scrollView
-    }()
-    
-    private lazy var imageHstackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        
-        // 이미지 추가
-        onboardingImageNames.forEach { imageName in
-            let imageView = UIImageView(image: imageName)
-            imageView.contentMode = .scaleAspectFit
-            stackView.addArrangedSubview(imageView)
-        }
-        return stackView
-    }()
-    
+    private let onboardingView = OnboardingView()
     private let bottomView = CustomBottomView(
         backButtonState: .enabled,
         nextButtonEnabled: true,
@@ -89,15 +49,22 @@ final class OnboardingViewController: UIViewController {
     
     private func setupUI() {
         view.backgroundColor = .gray02
-        [   pageControl,
-            scrollView,
+        [
+            onboardingView,
             bottomView
         ].forEach(view.addSubview(_:))
-        scrollView.addSubview(imageHstackView)
+        onboardingView.scrollView.delegate = self
         setupAutoLayout()
     }
     
     private func bind() {
+        onboardingView.currentPagePublisher
+            .removeDuplicates()
+            .sink { [weak self] pageNumber in
+                self?.updateNextButtonTitle(forPage: pageNumber)
+            }
+            .store(in: &cancellables)
+        
         bottomView.backButtonTappedPublisher
             .sink { [weak self] _ in
                 self?.showHomeVC()
@@ -109,30 +76,21 @@ final class OnboardingViewController: UIViewController {
                 self?.handleNextButtonTapped()
             }
             .store(in: &cancellables)
-        
-        currentPageSubject
-            .removeDuplicates()
-            .sink { [weak self] pageNumber in
-                guard let self = self else { return }
-                self.pageControl.currentPage = pageNumber
-                let buttonTitle = pageNumber >= self.onboardingImageNames.count - 1 ? Attributes.startText : Attributes.nextText
-                self.bottomView.setNextButtonTitle(buttonTitle)
-            }
-            .store(in: &cancellables)
+    }
+    
+    private func updateNextButtonTitle(forPage pageNumber: Int) {
+        let buttonTitle = pageNumber >= onboardingView.onboardingImagesCount - 1 ? Attributes.startText : Attributes.nextText
+        bottomView.setNextButtonTitle(buttonTitle)
     }
     
     private func handleNextButtonTapped() {
-        let nextPage = currentPageSubject.value + 1
+        let currentPage = onboardingView.currentPage
+        let nextPage = currentPage + 1
         
-        if nextPage >= onboardingImageNames.count {
+        if nextPage >= onboardingView.onboardingImagesCount {
             showHomeVC()
         } else {
-            UIView.animate(withDuration: 0.3) {
-                let offsetX = CGFloat(nextPage) * self.scrollView.frame.width
-                self.scrollView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: false)
-            } completion: { _ in
-                self.currentPageSubject.send(nextPage)
-            }
+            onboardingView.setPage(nextPage)
         }
     }
     
@@ -150,21 +108,10 @@ final class OnboardingViewController: UIViewController {
 
 extension OnboardingViewController {
     private func setupAutoLayout() {
-        pageControl.snp.makeConstraints {
+        onboardingView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(Metric.verticalSpacing)
-            $0.centerX.equalToSuperview()
-        }
-        
-        scrollView.snp.makeConstraints {
-            $0.top.equalTo(pageControl.snp.bottom).offset(Metric.verticalSpacing)
             $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(scrollView.snp.width).multipliedBy(Metric.scrollViewHeightMultiplier)
-        }
-        
-        imageHstackView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-            $0.height.equalTo(scrollView.snp.height)
-            $0.width.equalTo(scrollView.snp.width).multipliedBy(onboardingImageNames.count)
+            $0.bottom.equalTo(bottomView.snp.bottom)
         }
         
         bottomView.snp.makeConstraints {
@@ -178,8 +125,8 @@ extension OnboardingViewController {
 
 extension OnboardingViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
-        currentPageSubject.send(Int(pageIndex))
+        let pageIndex = round(scrollView.contentOffset.x / onboardingView.frame.width)
+        onboardingView.updateCurrentPage(to: Int(pageIndex))
     }
 }
 
