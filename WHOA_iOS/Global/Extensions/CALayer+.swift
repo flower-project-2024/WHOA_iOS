9c365ea7970f7b7a27bd2024e2050538b90a4150
Suyeon <syhhwang1231@gmail.com>
2024-10-06 22:58:21 +0900

    Feat: UIBezierPath를 이용해서 그림자 설정하는 extension 추가

--- WHOA_iOS/Global/Extensions/CALayer+.swift
+++ WHOA_iOS/Global/Extensions/CALayer+.swift
@@ -20,4 +20,24 @@ extension CALayer {
         shadowOffset = CGSize(width: width, height: height)
         shadowRadius = blur
     }
+    
+    func applyShadowByUIBezierPath(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        offsetWidth: CGFloat = 0,
+        offsetHeight: CGFloat = 0,
+        shadowWidth: CGFloat = 0,
+        shadowHeight: CGFloat = 2,
+        blur: CGFloat = 4,
+        x: CGFloat = 0,
+        y: CGFloat = 0
+    ) {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: offsetWidth, height: offsetHeight)
+        shadowRadius = blur
+        let shadowRect = CGRect(x: x, y: y, width: shadowWidth, height: shadowHeight)
+        let shadowPath = UIBezierPath(rect: shadowRect).cgPath
+        self.shadowPath = shadowPath
+    }
 }
