bb17a9e5079a6f57c95c0a6422d9066afa6b35b8
Hun <ksh26271@naver.com>
2024-11-23 06:02:43 +0900

    refactor: 이미지 선택 관련 UI PhotoSelectionView로 분리

--- WHOA_iOS/UI/FlowerCustomizing/PhotoSelection/PhotoSelectionViewController.swift
+++ WHOA_iOS/UI/FlowerCustomizing/PhotoSelection/PhotoSelectionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 import Photos
 
 final class PhotoSelectionViewController: UIViewController {
@@ -15,8 +16,10 @@ final class PhotoSelectionViewController: UIViewController {
     /// Metrics
     private enum Metric {
         static let sideMargin = 20.0
+        static let requirementLabelTopOffset = 30.0
         static let requirementTextViewTopOffset = 16.0
         static let requirementTextViewHeightMultiplier = 0.34
+        static let photoLabelTopOffset = 40.0
     }
     
     /// Attributes
@@ -43,117 +46,7 @@ final class PhotoSelectionViewController: UIViewController {
     private lazy var requirementLabel = buildLabel(text: Attributes.requirementLabelText)
     private let requirementTextView = RequirementTextView()
     private lazy var photoLabel = buildLabel(text: Attributes.photoLabelText)
-    
-    private lazy var addImageButton: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "plus.circle")
-        imageView.tintColor = .black
-        imageView.contentMode = .center
-        imageView.layer.cornerRadius = 10
-        imageView.layer.masksToBounds = true
-        imageView.backgroundColor = .gray02
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(photoImageViewTapped))
-        imageView.addGestureRecognizer(tapGesture)
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private let photoImageView1: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "PhotoIcon")
-        imageView.contentMode = .center
-        imageView.layer.cornerRadius = 10
-        imageView.layer.masksToBounds = true
-        imageView.backgroundColor = .gray02
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private lazy var minusImageView1: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "MinusButton")
-        imageView.tintColor = .black
-        imageView.contentMode = .scaleAspectFill
-        imageView.isHidden = true
-        imageView.tag = 1
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(minusImageViewTapped))
-        imageView.addGestureRecognizer(tapGesture)
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private let photoImageView2: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "PhotoIcon")
-        imageView.contentMode = .center
-        imageView.layer.cornerRadius = 10
-        imageView.layer.masksToBounds = true
-        imageView.backgroundColor = .gray02
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private lazy var minusImageView2: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "MinusButton")
-        imageView.tintColor = .black
-        imageView.contentMode = .scaleAspectFill
-        imageView.isHidden = true
-        imageView.tag = 2
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(minusImageViewTapped))
-        imageView.addGestureRecognizer(tapGesture)
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private let photoImageView3: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "PhotoIcon")
-        imageView.contentMode = .center
-        imageView.layer.cornerRadius = 10
-        imageView.layer.masksToBounds = true
-        imageView.backgroundColor = .gray02
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private lazy var minusImageView3: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "MinusButton")
-        imageView.tintColor = .black
-        imageView.contentMode = .scaleAspectFill
-        imageView.isHidden = true
-        imageView.tag = 3
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(minusImageViewTapped))
-        imageView.addGestureRecognizer(tapGesture)
-        imageView.isUserInteractionEnabled = true
-        return imageView
-    }()
-    
-    private lazy var photoImageViewHStackView: UIStackView = {
-        let stackView = UIStackView()
-        [
-            addImageButton,
-            photoImageView1,
-            photoImageView2,
-            photoImageView3
-        ].forEach { stackView.addArrangedSubview($0)}
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.spacing = 12
-        return stackView
-    }()
-    
-    private let photoImageHScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.showsHorizontalScrollIndicator = false
-        return scrollView
-    }()
-    
+    private let photoSelectionView = PhotoSelectionView()
     private let bottomView = CustomBottomView(backButtonState: .enabled, nextButtonEnabled: true)
     
     // MARK: - Initialize
@@ -172,7 +65,7 @@ final class PhotoSelectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        configUI()
+//        configUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -194,57 +87,51 @@ final class PhotoSelectionViewController: UIViewController {
             headerView,
             requirementLabel,
             requirementTextView,
+            photoLabel,
+            photoSelectionView,
             bottomView
             
         ].forEach(view.addSubview(_:))
-        
-        view.addSubview(photoLabel)
-        view.addSubview(photoImageHScrollView)
-        photoImageHScrollView.addSubview(photoImageViewHStackView)
-        
-        photoImageView1.addSubview(minusImageView1)
-        photoImageView2.addSubview(minusImageView2)
-        photoImageView3.addSubview(minusImageView3)
-        
         setupAutoLayout()
     }
     
-    private func configUI() {
-        updateImageViews(with: viewModel.photoSelectionModel.photoDatas)
-        updateMinusImageViews()
-    }
+//    private func configUI() {
+//        updateImageViews(with: viewModel.photoSelectionModel.photoDatas)
+//        updateMinusImageViews()
+//    }
     
-    private func resetImageViews() {
-        photoImageView1.image = UIImage(named: "PhotoIcon")
-        photoImageView2.image = UIImage(named: "PhotoIcon")
-        photoImageView3.image = UIImage(named: "PhotoIcon")
-        photoImageView1.contentMode = .center
-        photoImageView2.contentMode = .center
-        photoImageView3.contentMode = .center
-    }
+//    private func resetImageViews() {
+//        photoImageView1.image = UIImage(named: "PhotoIcon")
+//        photoImageView2.image = UIImage(named: "PhotoIcon")
+//        photoImageView3.image = UIImage(named: "PhotoIcon")
+//        photoImageView1.contentMode = .center
+//        photoImageView2.contentMode = .center
+//        photoImageView3.contentMode = .center
+//    }
     
-    private func updateImageViews(with photos: [Data]) {
-        for (index, data) in photos.enumerated() {
-            switch index {
-            case 0:
-                photoImageView1.image = UIImage(data: data)
-                photoImageView1.contentMode = .scaleAspectFill
-            case 1:
-                photoImageView2.image = UIImage(data: data)
-                photoImageView2.contentMode = .scaleAspectFill
-            case 2:
-                photoImageView3.image = UIImage(data: data)
-                photoImageView3.contentMode = .scaleAspectFill
-            default:
-                break
-            }
-        }
-    }
-    private func updateMinusImageViews() {
-        minusImageView1.isHidden = photoImageView1.image == UIImage(named: "PhotoIcon")
-        minusImageView2.isHidden = photoImageView2.image == UIImage(named: "PhotoIcon")
-        minusImageView3.isHidden = photoImageView3.image == UIImage(named: "PhotoIcon")
-    }
+//    private func updateImageViews(with photos: [Data]) {
+//        for (index, data) in photos.enumerated() {
+//            switch index {
+//            case 0:
+//                photoImageView1.image = UIImage(data: data)
+//                photoImageView1.contentMode = .scaleAspectFill
+//            case 1:
+//                photoImageView2.image = UIImage(data: data)
+//                photoImageView2.contentMode = .scaleAspectFill
+//            case 2:
+//                photoImageView3.image = UIImage(data: data)
+//                photoImageView3.contentMode = .scaleAspectFill
+//            default:
+//                break
+//            }
+//        }
+//    }
+    
+//    private func updateMinusImageViews() {
+//        minusImageView1.isHidden = photoImageView1.image == UIImage(named: "PhotoIcon")
+//        minusImageView2.isHidden = photoImageView2.image == UIImage(named: "PhotoIcon")
+//        minusImageView3.isHidden = photoImageView3.image == UIImage(named: "PhotoIcon")
+//    }
     
     private func buildLabel(text: String) -> UILabel {
         let label = UILabel()
@@ -256,69 +143,69 @@ final class PhotoSelectionViewController: UIViewController {
     
     // MARK: - Actions
     
-    @objc
-    func minusImageViewTapped(_ sender: UITapGestureRecognizer) {
-        let idx = sender.view?.tag == 1 ? 0 : sender.view?.tag == 2 ? 1 : 2
-        
-        viewModel.photoSelectionModel.photoDatas.remove(at: idx)
-        resetImageViews()
-        updateImageViews(with: viewModel.getPhotosArray())
-        updateMinusImageViews()
-    }
-    
-    @objc
-    func photoImageViewTapped() {
-        viewModel.authService.requestAuthorization { [weak self] result in
-            guard let self else { return }
-            
-            switch result {
-            case .success:
-                let vc = PhotoViewController(photosCount: viewModel.getPhotosCount(), photoSelectionLimitCount: 3)
-                vc.modalPresentationStyle = .fullScreen
-                vc.completionHandler = { photos in
-                    let photoDatas = photos.values.compactMap{ $0 }.compactMap{ $0.pngData() }
-                    self.viewModel.addPhotos(photos: photoDatas)
-                    
-                    for i in 0..<self.viewModel.getPhotosCount() {
-                        switch i {
-                        case 0:
-                            self.photoImageView1.image = UIImage(data: self.viewModel.getPhoto(idx: i))
-                            self.photoImageView1.contentMode = .scaleAspectFill
-                            self.minusImageView1.isHidden = false
-                        case 1:
-                            self.photoImageView2.image = UIImage(data: self.viewModel.getPhoto(idx: i))
-                            self.photoImageView2.contentMode = .scaleAspectFill
-                            self.minusImageView2.isHidden = false
-                        case 2:
-                            self.photoImageView3.image = UIImage(data: self.viewModel.getPhoto(idx: i))
-                            self.photoImageView3.contentMode = .scaleAspectFill
-                            self.minusImageView3.isHidden = false
-                        default:
-                            continue
-                        }
-                    }
-                    
-                    switch self.viewModel.getPhotosCount() {
-                    case 1:
-                        self.photoImageView3.image = UIImage(named: "PhotoIcon")
-                        self.photoImageView3.contentMode = .center
-                    case 0:
-                        self.photoImageView3.image = UIImage(named: "PhotoIcon")
-                        self.photoImageView2.image = UIImage(named: "PhotoIcon")
-                        self.photoImageView3.contentMode = .center
-                        self.photoImageView2.contentMode = .center
-                    default:
-                        break
-                    }
-                }
-                
-                present(vc, animated: true)
-                
-            case .failure:
-                return
-            }
-        }
-    }
+//    @objc
+//    func minusImageViewTapped(_ sender: UITapGestureRecognizer) {
+//        let idx = sender.view?.tag == 1 ? 0 : sender.view?.tag == 2 ? 1 : 2
+//        
+//        viewModel.photoSelectionModel.photoDatas.remove(at: idx)
+//        resetImageViews()
+//        updateImageViews(with: viewModel.getPhotosArray())
+//        updateMinusImageViews()
+//    }
+//    
+//    @objc
+//    func photoImageViewTapped() {
+//        viewModel.authService.requestAuthorization { [weak self] result in
+//            guard let self else { return }
+//            
+//            switch result {
+//            case .success:
+//                let vc = PhotoViewController(photosCount: viewModel.getPhotosCount(), photoSelectionLimitCount: 3)
+//                vc.modalPresentationStyle = .fullScreen
+//                vc.completionHandler = { photos in
+//                    let photoDatas = photos.values.compactMap{ $0 }.compactMap{ $0.pngData() }
+//                    self.viewModel.addPhotos(photos: photoDatas)
+//                    
+//                    for i in 0..<self.viewModel.getPhotosCount() {
+//                        switch i {
+//                        case 0:
+//                            self.photoImageView1.image = UIImage(data: self.viewModel.getPhoto(idx: i))
+//                            self.photoImageView1.contentMode = .scaleAspectFill
+//                            self.minusImageView1.isHidden = false
+//                        case 1:
+//                            self.photoImageView2.image = UIImage(data: self.viewModel.getPhoto(idx: i))
+//                            self.photoImageView2.contentMode = .scaleAspectFill
+//                            self.minusImageView2.isHidden = false
+//                        case 2:
+//                            self.photoImageView3.image = UIImage(data: self.viewModel.getPhoto(idx: i))
+//                            self.photoImageView3.contentMode = .scaleAspectFill
+//                            self.minusImageView3.isHidden = false
+//                        default:
+//                            continue
+//                        }
+//                    }
+//                    
+//                    switch self.viewModel.getPhotosCount() {
+//                    case 1:
+//                        self.photoImageView3.image = UIImage(named: "PhotoIcon")
+//                        self.photoImageView3.contentMode = .center
+//                    case 0:
+//                        self.photoImageView3.image = UIImage(named: "PhotoIcon")
+//                        self.photoImageView2.image = UIImage(named: "PhotoIcon")
+//                        self.photoImageView3.contentMode = .center
+//                        self.photoImageView2.contentMode = .center
+//                    default:
+//                        break
+//                    }
+//                }
+//                
+//                present(vc, animated: true)
+//                
+//            case .failure:
+//                return
+//            }
+//        }
+//    }
     
     @objc
     private func backButtonTapped() {
@@ -343,8 +230,8 @@ extension PhotoSelectionViewController {
         }
         
         requirementLabel.snp.makeConstraints {
-            $0.top.equalTo(headerView.snp.bottom).offset(30)
-            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(headerView.snp.bottom).offset(Metric.requirementLabelTopOffset)
+            $0.leading.equalToSuperview().offset(Metric.sideMargin)
         }
         
         requirementTextView.snp.makeConstraints {
@@ -354,33 +241,14 @@ extension PhotoSelectionViewController {
         }
         
         photoLabel.snp.makeConstraints {
-            $0.top.equalTo(requirementTextView.snp.bottom).offset(40)
-            $0.leading.equalToSuperview().offset(20)
-            $0.trailing.equalToSuperview().inset(280)
+            $0.top.equalTo(requirementTextView.snp.bottom).offset(Metric.photoLabelTopOffset)
+            $0.leading.equalToSuperview().offset(Metric.sideMargin)
         }
         
-        photoImageView1.snp.makeConstraints {
-            $0.width.equalTo(130.adjusted())
-            $0.height.equalTo(136.adjustedH())
-        }
-        
-        photoImageViewHStackView.snp.makeConstraints {
-            $0.edges.equalTo(photoImageHScrollView)
-            $0.height.equalTo(photoImageHScrollView)
-        }
-        
-        photoImageHScrollView.snp.makeConstraints {
-            $0.top.equalTo(photoLabel.snp.bottom).offset(16)
-            $0.leading.equalToSuperview().offset(20)
+        photoSelectionView.snp.makeConstraints {
+            $0.top.equalTo(photoLabel.snp.bottom).offset(Metric.requirementTextViewTopOffset)
+            $0.leading.equalToSuperview().offset(Metric.sideMargin)
             $0.trailing.equalToSuperview()
-        }
-        
-        [minusImageView1, minusImageView2, minusImageView3].forEach { imageView in
-            imageView.snp.makeConstraints {
-                $0.top.equalToSuperview().offset(5)
-                $0.trailing.equalToSuperview().offset(-5)
-                $0.size.equalTo(16)
-            }
         }
         
         bottomView.snp.makeConstraints {
---
+++ WHOA_iOS/UI/FlowerCustomizing/PhotoSelection/Views/PhotoSelectionView.swift
@@ -0,0 +1,191 @@
+//
+//  PhotoSelectionView.swift
+//  WHOA_iOS
+//
+//  Created by KSH on 11/22/24.
+//
+
+import UIKit
+
+final class PhotoSelectionView: UIView {
+    
+    // MARK: - Enums
+    
+    /// Metrics
+    private enum Metric {
+        static let imageViewWidth = UIScreen.main.bounds.width > 375 ? 150.0 : 130.0
+        static let imageViewHeightMultiplier = 1.04
+        static let minusButtonInset = 5.0
+        static let minusButtonSize = 16.0
+    }
+    
+    /// Attributes
+    private enum Attributes {
+        static let plusImage = UIImage(systemName: "plus.circle")
+        static let photoIcon = UIImage(named: "PhotoIcon")
+        static let minusButton = UIImage(named: "MinusButton")
+    }
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.alwaysBounceVertical = false
+        return scrollView
+    }()
+    
+    private lazy var addImageButton: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.plusImage,
+            backgroundColor: .gray02,
+            tintColor: .black
+        )
+        // 터치 이벤트 필요
+        return imageView
+    }()
+    
+    private lazy var photoImageView1: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.photoIcon,
+            backgroundColor: .gray02
+        )
+        return imageView
+    }()
+    
+    private lazy var minusImageView1: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.minusButton,
+            contentMode: .scaleAspectFill,
+            cornerRadius: 0,
+            isHidden: true
+        )
+        // 터치 이벤트 필요
+        return imageView
+    }()
+    
+    private lazy var photoImageView2: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.photoIcon,
+            backgroundColor: .gray02
+        )
+        return imageView
+    }()
+    
+    private lazy var minusImageView2: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.minusButton,
+            contentMode: .scaleAspectFill,
+            cornerRadius: 0,
+            isHidden: true
+        )
+        // 터치 이벤트 필요
+        return imageView
+    }()
+    
+    private lazy var photoImageView3: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.photoIcon,
+            backgroundColor: .gray02
+        )
+        return imageView
+    }()
+    
+    private lazy var minusImageView3: UIImageView = {
+        let imageView = buildImageView(
+            image: Attributes.minusButton,
+            contentMode: .scaleAspectFill,
+            cornerRadius: 0,
+            isHidden: true
+        )
+        // 터치 이벤트 필요
+        return imageView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            addImageButton,
+            photoImageView1,
+            photoImageView2,
+            photoImageView3
+        ])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 12
+        return stackView
+    }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Functions
+    
+    private func setupUI() {
+        backgroundColor = .white
+        
+        addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        photoImageView1.addSubview(minusImageView1)
+        photoImageView2.addSubview(minusImageView2)
+        photoImageView3.addSubview(minusImageView3)
+        
+        setupAutoLayout()
+    }
+    
+    private func buildImageView(
+        image: UIImage?,
+        backgroundColor: UIColor? = .clear,
+        tintColor: UIColor? = nil,
+        contentMode: UIView.ContentMode = .center,
+        cornerRadius: CGFloat = 10,
+        isHidden: Bool = false
+    ) -> UIImageView {
+        let imageView = UIImageView()
+        imageView.image = image
+        imageView.tintColor = tintColor
+        imageView.backgroundColor = backgroundColor
+        imageView.contentMode = contentMode
+        imageView.layer.cornerRadius = cornerRadius
+        imageView.layer.masksToBounds = cornerRadius > 0
+        imageView.isHidden = isHidden
+        return imageView
+    }
+}
+
+// MARK: - AutoLayout
+
+extension PhotoSelectionView {
+    private func setupAutoLayout() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.edges.height.equalToSuperview()
+        }
+        
+        photoImageView1.snp.makeConstraints {
+            $0.width.equalTo(Metric.imageViewWidth)
+            $0.height.equalTo(photoImageView1.snp.width).multipliedBy(Metric.imageViewHeightMultiplier)
+        }
+        
+        [minusImageView1, minusImageView2, minusImageView3].forEach { imageView in
+            imageView.snp.makeConstraints {
+                $0.top.trailing.equalToSuperview().inset(Metric.minusButtonInset)
+                $0.size.equalTo(Metric.minusButtonSize)
+            }
+        }
+    }
+}
+
